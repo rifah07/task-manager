b66dd018714de727707bbd8870cb73dc8f154308
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-09-19 23:11:21 +0600

    Tasklist & TaskInput added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TaskInput from './TaskTnput';
+import TaskList from './TaskLIst';
 
 function App() {
+  const [tasks, setTasks]= useState([]);
+  const [newTask, setNewTask]= useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task Manager</h1>
+      <TaskInput />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
---
+++ src/TaskTnput.jsx
@@ -0,0 +1,9 @@
+function TaskInput(props) {
+  return (
+    <div>
+      <input type="text" placeholder="Enter a new Task" />
+      <button>Add Task</button>
+    </div>
+  );
+}
+export default TaskInput;
